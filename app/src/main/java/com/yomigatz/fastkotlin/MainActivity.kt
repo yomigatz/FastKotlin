4335df89c391a7c5e071a4d76acc7f42c374ed87
yomigatz <yomigatz@163.com>
2022-04-02 16:39:16 +0800

    0402

--- app/src/main/java/com/yomigatz/fastkotlin/MainActivity.kt
+++ app/src/main/java/com/yomigatz/fastkotlin/MainActivity.kt
@@ -3,7 +3,11 @@ package com.yomigatz.fastkotlin
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.util.Log
+import android.view.View
 import android.widget.ListView
+import kotlinx.coroutines.*
+import kotlin.coroutines.resume
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,4 +19,38 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
     }
+
+    fun onCancelCoroutine(view: View) {
+        GlobalScope.launch(Dispatchers.IO) {
+            val test = getData()
+            Log.d("NRRR","onCancelCoroutine+++++$test")
+        }
+    }
+
+    suspend fun getData():String? = suspendCancellableCoroutine{
+        cancellableCoroutine->
+        val test = TestCoroutine()
+        test.setCallback(object :TestCoroutine.TestCallback{
+            override fun onSuccess(succeed: String) {
+                Log.d("NRRR","onSuccess+++++")
+                cancellableCoroutine.resumeIfActive(succeed)
+            }
+            override fun onSuccess2(succeed: String) {
+                Log.d("NRRR","onSuccess2+++++")
+                cancellableCoroutine.resumeIfActive(succeed)
+            }
+            override fun onFailed() {
+                Log.d("NRRR","onFailed+++++")
+                cancellableCoroutine.resumeIfActive("++++++")
+            }
+        })
+        test.loading()
+    }
+
+    private fun <T> CancellableContinuation<T>.resumeIfActive(value: T) {
+        if (isActive) {
+            resume(value)
+        }
+    }
 }
+
---
+++ app/src/main/java/com/yomigatz/fastkotlin/TestCoroutine.kt
@@ -0,0 +1,37 @@
+package com.yomigatz.fastkotlin
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+/**
+ * Author: umi
+ * Date: 2022/4/1 11:51
+ * Description: This is TestCoroutine.
+ */
+class TestCoroutine {
+
+    private var callback: TestCallback? = null
+
+    fun setCallback(callback: TestCallback) {
+        this.callback = callback
+    }
+
+    fun loading() {
+        GlobalScope.launch(Dispatchers.IO) {
+            delay(2000)
+            callback?.onSuccess("onSuccess")
+            delay(3000)
+            callback?.onSuccess2("onSuccess2")
+            delay(2000)
+            callback?.onFailed()
+        }
+    }
+
+    interface TestCallback {
+        fun onSuccess(succeed: String)
+        fun onSuccess2(succeed: String)
+        fun onFailed()
+    }
+}
